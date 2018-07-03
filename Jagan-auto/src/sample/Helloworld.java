5bf330fa585c6196f7043a6685ba7d3653bc94f7
Jagan <chukka.jagannadh@gmail.com>
2018-07-03 19:26:30 +0530

    changed helloworld

--- Jagan-auto/src/sample/Helloworld.java
+++ Jagan-auto/src/sample/Helloworld.java
@@ -3,6 +3,7 @@ package sample;
 public class Helloworld {
 
 	public static void main(String[] args) {
-		System.out.println("Hello World_loop");
+		System.out.println("Hello World_loop change");
 	}
+	
 }
